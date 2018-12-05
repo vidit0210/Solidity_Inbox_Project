ae8ad18a3fcdc928a01017caf2b23150e833dc26
Vidit Shah <vidit0210@gmail.com>
2018-12-06 05:15:51 +0530

    Setup

---
+++ Contracts/Inbox.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.17;
+contract Inbox{
+    string public message;
+    function Inbox(string memory initialMessage) public {
+        message= initialMessage;
+    }
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+}
