1947690e541dda06378b004e5c23147fd193c7cc
Vidit Shah <vidit0210@gmail.com>
2018-12-06 18:34:47 +0530

    contracttest

--- Test/inbox.test.js
+++ Test/inbox.test.js
@@ -1,4 +1,17 @@
 const assert  = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');// Web3 in caps because it returns Constructor
+const web3 =  new Web3(ganache.provider());
 
+beforeEach(()=>{
+    //Get all Accounts by Ganache
+    web3.eth.getAccounts().then(fethedAccounts =>{
+        console.log(fethedAccounts);
+    })
+})
+
+describe('Inbox',()=>{
+    it('Deploys a Contract',()=>{
+
+    });
+})
